f25883c5e87d1ab0c567c0aeda4f4a8affcee07e
Sofia Mansilla <sofia.developer@proton.me>
2023-08-15 11:37:03 -0400

    first commit

---
+++ code.js
@@ -0,0 +1,89 @@
+/* --------------------------- NO TOCAR DESDE ACÁ --------------------------- */
+let datosPersona = {
+  nombre: "",
+  edad: 0,
+  ciudad: "",
+  interesPorJs: "",
+};
+
+const listado = [
+  {
+    imgUrl: "https://huguidugui.files.wordpress.com/2015/03/html1.png",
+    lenguajes: "HTML y CSS",
+    bimestre: "1er bimestre",
+  },
+  {
+    imgUrl: "https://jherax.files.wordpress.com/2018/08/javascript_logo.png",
+    lenguajes: "Javascript",
+    bimestre: "2do bimestre",
+  },
+  {
+    imgUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1200px-React.svg.png",
+    lenguajes: "React JS",
+    bimestre: "3er bimestre",
+  },
+];
+
+const profileBtn = document.querySelector("#completar-perfil");
+const materiasBtn = document.querySelector("#obtener-materias");
+const verMasBtn = document.querySelector("#ver-mas");
+const cambiarTema = document.querySelector("#cambiar-tema");
+
+profileBtn.addEventListener("click", renderizarDatosUsuario);
+materiasBtn.addEventListener("click", recorrerListadoYRenderizarTarjetas);
+cambiarTema.addEventListener("click", alternarColorTema);
+/* --------------------------- NO TOCAR HASTA ACÁ --------------------------- */
+
+function obtenerDatosDelUsuario() {
+  /* --------------- PUNTO 1: Escribe tu codigo a partir de aqui --------------- */
+
+  // Borrar luego: Pedir y validad nombre
+
+  let nombre = "null";
+let msjErrorNombre = "";
+
+do{
+    nombre = prompt(msjErrorNombre + "\nIngrese su nombre: ");
+    msjErrorNombre = "Ingrese un nombre valido";
+    } while(!isNaN(nombre) || nombre.length < 3);
+
+    datosPersona.nombre = nombre;
+
+console.log(nombre);
+
+
+    let edad = null;
+    let inputAnioNacimiento = null;
+    let msjError = "";
+  
+    // Solicitar Año de nacimiento y calcular la edad
+    do {
+      inputAnioNacimiento = prompt(msjError + "Ingrese su año de nacimiento: ");
+      edad = parseInt(inputAnioNacimiento);
+      msjError = "Error, el valor debe ser mayor a cero y númerico\n";
+    } while (isNaN(inputAnioNacimiento) || inputAnioNacimiento.length !== 4);
+  
+    datosPersona.edad = new Date().getFullYear() - inputAnioNacimiento 
+    console.log(inputAnioNacimiento);
+  
+console.log(datosPersona);
+
+};
+obtenerDatosDelUsuario();
+
+function renderizarDatosUsuario() {
+  /* ------------------- NO TOCAR NI ELIMINAR ESTA FUNCION. ------------------- */
+  obtenerDatosDelUsuario();
+  /* --------------- PUNTO 2: Escribe tu codigo a partir de aqui --------------- */
+}
+
+function recorrerListadoYRenderizarTarjetas() {
+  /* ------------------ PUNTO 3: Escribe tu codigo desde aqui ------------------ */
+}
+
+function alternarColorTema() {
+  /* --------------------- PUNTO 4: Escribe tu codigo aqui --------------------- */
+}
+
+/* --------------------- PUNTO 5: Escribe tu codigo aqui --------------------- */
